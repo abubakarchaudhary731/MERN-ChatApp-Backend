e0a559fc1cdcf5a72b2e930fd926f19f22a6fad0
Abu Bakar <abubakarchaudhary731@gmail.com>
2024-09-30 20:33:31 +0500

    update

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ const server = app.listen(PORT, () => {
 const io = require('socket.io')(server, {
     pingTimeout: 60000,
     cors: {
-        origin: "http://localhost:5173",
+        origin: process.env.Front_End_URL,
     },
 });
 
